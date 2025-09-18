14052c91ad16337b4e911aadd81f520d49ee5db9
qwaszxhjhj <natewang@outlook.com>
2025-09-18 17:34:24 -0400

    finished ver1

--- movingcompany/src/app/app.routes.ts
+++ movingcompany/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'; // Make sure this import is correct
 import { ContactComponent } from './contact/contact.component';
+import { ProcedureComponent } from './procedure/procedure.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { TipsComponent } from './tips/tips.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // This makes HomeComponent the default for the root path
     { path: 'contact', component: ContactComponent },
+    { path: 'procedure', component: ProcedureComponent },
+    { path: 'aboutus', component: AboutusComponent },
+    { path: 'tips', component: TipsComponent },
 ];
---
+++ movingcompany/src/app/tips/tips.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tips',
+  imports: [],
+  templateUrl: './tips.component.html',
+  styleUrl: './tips.component.css'
+})
+export class TipsComponent {
+
+}
